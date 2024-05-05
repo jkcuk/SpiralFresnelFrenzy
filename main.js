71902172ef85854e624fc513f7b50529d1d92e3f
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-05-05 16:12:41 +0100

    more bug fixing attempts

--- main.js
+++ main.js
@@ -1080,28 +1080,28 @@ function addXRInteractivity() {
 
 	// the two hand controllers
 
-	const geometry = new THREE.BufferGeometry();
-	geometry.setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 5 ) ] );
+	// const geometry = new THREE.BufferGeometry();
+	// geometry.setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 5 ) ] );
 
-	const controller1 = renderer.xr.getController( 0 );
-	controller1.add( new THREE.Line( geometry ) );
-	scene.add( controller1 );
+	// const controller1 = renderer.xr.getController( 0 );
+	// controller1.add( new THREE.Line( geometry ) );
+	// scene.add( controller1 );
 
-	const controller2 = renderer.xr.getController( 1 );
-	controller2.add( new THREE.Line( geometry ) );
-	scene.add( controller2 );
+	// const controller2 = renderer.xr.getController( 1 );
+	// controller2.add( new THREE.Line( geometry ) );
+	// scene.add( controller2 );
 
 	//
 
-	const controllerModelFactory = new XRControllerModelFactory();
+	// const controllerModelFactory = new XRControllerModelFactory();
 
-	const controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-	controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-	scene.add( controllerGrip1 );
+	// const controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+	// controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+	// scene.add( controllerGrip1 );
 
-	const controllerGrip2 = renderer.xr.getControllerGrip( 1 );
-	controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
-	scene.add( controllerGrip2 );
+	// const controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+	// controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+	// scene.add( controllerGrip2 );
 
 	//
 
@@ -1117,7 +1117,7 @@ function addXRInteractivity() {
 	mesh.position.z = - 0.5;
 	mesh.rotation.y = Math.PI / 4;
 	mesh.scale.setScalar( 2 );
-	group.add( mesh );	
+	// group.add( mesh );	
 }
 
 // function updateGUI() {
