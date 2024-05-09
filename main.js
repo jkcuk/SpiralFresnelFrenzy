96f6da1c5182f92b7ee280a249cf74ce6c0040e5
jkcuk <44874423+jkcuk@users.noreply.github.com>
2024-05-09 14:23:23 +0100

    Trying to use controller(s)

--- main.js
+++ main.js
@@ -1081,28 +1081,28 @@ function addXRInteractivity() {
 
 	// the two hand controllers
 
-	// const geometry = new THREE.BufferGeometry();
-	// geometry.setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 5 ) ] );
+	const geometry = new THREE.BufferGeometry();
+	geometry.setFromPoints( [ new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 0, - 5 ) ] );
 
-	// const controller1 = renderer.xr.getController( 0 );
-	// controller1.add( new THREE.Line( geometry ) );
-	// scene.add( controller1 );
+	const controller1 = renderer.xr.getController( 0 );
+	controller1.add( new THREE.Line( geometry ) );
+	scene.add( controller1 );
 
-	// const controller2 = renderer.xr.getController( 1 );
-	// controller2.add( new THREE.Line( geometry ) );
-	// scene.add( controller2 );
+	const controller2 = renderer.xr.getController( 1 );
+	controller2.add( new THREE.Line( geometry ) );
+	scene.add( controller2 );
 
 	//
 
-	// const controllerModelFactory = new XRControllerModelFactory();
+	const controllerModelFactory = new XRControllerModelFactory();
 
-	// const controllerGrip1 = renderer.xr.getControllerGrip( 0 );
-	// controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
-	// scene.add( controllerGrip1 );
+	const controllerGrip1 = renderer.xr.getControllerGrip( 0 );
+	controllerGrip1.add( controllerModelFactory.createControllerModel( controllerGrip1 ) );
+	scene.add( controllerGrip1 );
 
-	// const controllerGrip2 = renderer.xr.getControllerGrip( 1 );
-	// controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
-	// scene.add( controllerGrip2 );
+	const controllerGrip2 = renderer.xr.getControllerGrip( 1 );
+	controllerGrip2.add( controllerModelFactory.createControllerModel( controllerGrip2 ) );
+	scene.add( controllerGrip2 );
 
 	//
 
